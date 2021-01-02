73095bc4dc834fa9cef814ed334acf9d4f8ad535
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2021-01-02 02:09:56 +0200

    Access bug fixed

--- controllers/PagesController.php
+++ controllers/PagesController.php
@@ -69,7 +69,7 @@ class PagesController extends Controller
                 'class' => AccessControl::class,
                 'rules' => [
                     [
-                        'actions' => ['update', 'create'],
+                        'actions' => ['update', 'create', 'delete'],
                         'roles' => ['updatePosts'],
                         'allow' => true
                     ], [
